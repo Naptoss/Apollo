922101677df02214ab337881f45efdf3a79ccadf
Naptoss <tomgabinio@gmail.com>
2022-10-05 15:58:33 -0300

    Registrado e criado o caminho do "sobre"

--- Perseu/urls.py
+++ Perseu/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Dionisio/', include('Dionisio.urls')),
+    path('sobre/', include('sobre.urls'))
 ]
